a8f4ec7159394db458dfaf56859d55f4519ce538
KelBornn <bryanauthiat1@gmail.com>
2021-12-11 20:25:46 +0100

    change ssh key path

--- terraform/terraform-files/main.tf
+++ terraform/terraform-files/main.tf
@@ -67,3 +67,7 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server.*.public_ip
 }
+
+output userdata {
+  value = "\n${data.template_file.user_data.rendered}"
+}
